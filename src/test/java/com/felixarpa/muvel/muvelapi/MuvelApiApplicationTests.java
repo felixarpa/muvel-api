bf8549847923bfc58c8c17b762383490cf7d10f2
Felix Arribas Pardo <felix.arribas@skyscanner.net>
2022-04-04 19:13:42 +0200

    Init project

---
+++ src/test/java/com/felixarpa/muvel/muvelapi/MuvelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felixarpa.muvel.muvelapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuvelApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
